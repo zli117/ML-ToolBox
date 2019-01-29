fbc2a7b6f4183a1cd9dcfaf29d94b226371ed663
Zonlgin-Li6565 <development.my6565@gmail.com>
2019-01-29 12:25:45 -0600

    Adding missing type hints

--- toolbox/callbacks/progress_bar.py
+++ toolbox/callbacks/progress_bar.py
@@ -13,18 +13,18 @@ class ProgressBarCB(CallBack):
     _progress_bar: Optional[ProgressBar] = field(init=False, default=None)
     _curr_epoch: int = field(init=False, default=0)
 
-    def on_train_epoch_begin(self, curr_epoch: int, total_epochs: int):
+    def on_train_epoch_begin(self, curr_epoch: int, total_epochs: int) -> None:
         self._curr_epoch = curr_epoch
         self._progress_bar = ProgressBar(self.progress_bar_size,
                                          ' loss: %.06f, batch: %d, epoch: %d')
 
     def on_train_batch_end(self, curr_step: int, total_steps: int,
-                           loss: torch.Tensor):
+                           loss: torch.Tensor) -> None:
         if self._progress_bar is not None:
             self._progress_bar.progress(curr_step / total_steps * 100, loss,
                                         self._curr_epoch)
 
-    def on_train_epoch_end(self, curr_epoch: int, total_epochs: int):
+    def on_train_epoch_end(self, curr_epoch: int, total_epochs: int) -> None:
         # Print a new line at the end of each epoch
         print()
 
--- toolbox/utils/misc.py
+++ toolbox/utils/misc.py
@@ -1,17 +1,20 @@
+from typing import Union, Optional
+
 import torch
 from torch import nn
 
 
-def cuda(x):
+def cuda(x: Union[torch.Tensor, nn.Module]) -> Union[torch.Tensor, nn.Module]:
     return x.cuda() if torch.cuda.is_available() else x
 
 
-def save_model(model: nn.Module, file_path, epoch, seed, step=None):
+def save_model(model: nn.Module, file_path: str, epoch: int, seed: int,
+               step: Optional[int] = 0) -> None:
     torch.save({'model': model.state_dict(), 'epoch': epoch, 'seed': seed,
                 'step': step}, file_path)
 
 
-def load_model(file_path, model: nn.Module):
+def load_model(file_path: str, model: nn.Module) -> nn.Module:
     obj = torch.load(file_path)
     model_state = obj['model']
     model.load_state_dict(model_state)
